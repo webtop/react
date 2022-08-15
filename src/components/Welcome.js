3c9ed6e8a9c04d909d9f42715e5f888f47c2ae19
Paul Allsopp <pallsopp@gmail.com>
2022-08-15 16:39:26 -0700

    Moving changleable data to JSON files

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,20 +1,17 @@
 import React from "react";
+import welcomeImages from './data/welcome_images.json';
 
 const Welcome = () => {
     return (
       <div className="scene" id="welcome">
           <article className="content">
               <div className="gallery">
-                  <img src="https://landonhotel.com/images/hotel/intro_room.jpg"
-                       alt="Intro Gallery Room Sample Pictures"/>
-                  <img src="https://landonhotel.com/images/hotel/intro_pool.jpg"
-                       alt="Intro Gallery Pool Sample Pictures"/>
-                  <img src="https://landonhotel.com/images/hotel/intro_dining.jpg"
-                       alt="Intro Gallery Dining Sample Pictures"/>
-                  <img src="https://landonhotel.com/images/hotel/intro_attractions.jpg"
-                       alt="Intro Gallery Attractions Sample Pictures"/>
-                  <img className="hidesm" src="https://landonhotel.com/images/hotel/intro_wedding.jpg"
-                       alt="Intro Gallery Dining Sample Pictures"/>
+                  {
+                      welcomeImages.map((image) =>
+                        <img className={image.class} src={`https://landonhotel.com/images/hotel/intro_${image.src}.jpg`}
+                             alt={`Intro Gallery ${image.alt} Sample Pictures`} />
+                      )
+                  }
               </div>
               <h1>Welcome to the Landon&nbsp;Hotel</h1>
               <p>The original Landon perseveres after 50 years in the heart of West London. The West End
